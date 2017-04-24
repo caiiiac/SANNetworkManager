3dcb76f3168f2e2b0854b9cde436a5d4f06db023
caiiiac <caiiiac@163.com>
2017-04-24 17:06:46 +0800

    post

--- SANNetworkManager/SANNetworkManager/SANNetworkManager.swift
+++ SANNetworkManager/SANNetworkManager/SANNetworkManager.swift
@@ -6,8 +6,45 @@
 //  Copyright © 2017年 唐三彩. All rights reserved.
 //
 
-import UIKit
+import AFNetworking
+
+enum RequestType : String{
+    case GET = "GET"
+    case POST = "POST"
+}
 
 class SANNetworkManager: AFHTTPSessionManager {
 
+    static let shareInstance : SANNetworkManager = {
+       let manager = SANNetworkManager()
+        manager.responseSerializer.acceptableContentTypes?.insert("text/html")
+        
+        return manager
+    }()
+}
+
+
+//MARK: - 封装请求方法
+extension SANNetworkManager {
+    func request(methodType : RequestType, urlString : String, parameters : [String : Any], finished : @escaping
+        (Any?, Error?) -> ()) {
+    
+        //定义成功后的回调
+        let successCallBack = { (task : URLSessionDataTask, result : Any?) in
+            finished(result, nil)
+        }
+        //定义失败后的回调
+        let failureCallBack = { (task : URLSessionDataTask?, error : Error) in
+            finished(nil, error)
+        }
+        
+        
+        if methodType == .GET {
+            self.get(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        }
+        else
+        {
+            self.post(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        }
+    }
 }
